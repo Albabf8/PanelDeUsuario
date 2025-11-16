5834f16b73e58862033c14fd436efd6cb4c8ff19
Albabf8 <164393689+Albabf8@users.noreply.github.com>
2025-11-16 19:10:49 +0100

    ✅New: Creación formulario Registro

--- main.js
+++ main.js
@@ -3,12 +3,16 @@ import { showScene } from "./utils/utils.js";
 const botonRegistro = document.getElementById('botonRegistro');
 const botonIniciarSesion = document.getElementById('botonIniciarSesion');
 const botonCerrarSesion = document.getElementById('botonCerrarSesion');
+const enlaceInicioSesion = document.getElementById('enlaceInicioSesion');
+const enlaceRegistro = document.getElementById('enlaceRegistro');
 
 showScene('formularioRegistro');
 
 botonIniciarSesion.addEventListener('click', () => showScene('formularioPanelDeUsuario'));
 botonRegistro.addEventListener('click', () => showScene('formularioIniciarSesion'));
 botonCerrarSesion.addEventListener('click', () => showScene('formularioIniciarSesion'));
+enlaceInicioSesion.addEventListener('click', () => showScene('formularioIniciarSesion'));
+enlaceRegistro.addEventListener('click', () => showScene('formularioRegistro'));
 
 
 
